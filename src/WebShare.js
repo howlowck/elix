8e37221df588737f0760092f15c687ff461d399f
Hao Luo <howlowck@gmail.com>
2019-06-23 15:50:02 -0500

    added initial Webshare code

--- src/WebShare.js
+++ src/WebShare.js
@@ -1,3 +1,5 @@
+import * as symbols from './symbols.js';
+import * as template from './template.js';
 import Button from './Button.js';
 
 /**
@@ -8,55 +10,62 @@ import Button from './Button.js';
  * @inherits Button
  */
 class WebShare extends Button {
-    get defaultState() {
-        return Object.assign(super.defaultState, {
-            shareTitle: null,
-            shareText: null,
-            shareUrl: null
-        });
-    }
+  get defaultState() {
+    return Object.assign(super.defaultState, {
+      shareTitle: null,
+      shareText: null,
+      shareUrl: null
+    });
+  }
 
-    get shareTitle() {
-        return this.state.shareTitle;
-    }
+  get shareTitle() {
+    return this.state.shareTitle;
+  }
 
-    set shareTitle(shareTitle) {
-        this.setState({ shareTitle });
-    }
+  set shareTitle(shareTitle) {
+    this.setState({ shareTitle });
+  }
 
-    get shareText() {
-        return this.state.shareTitle;
-    }
+  get shareText() {
+    return this.state.shareText;
+  }
 
-    set shareText(shareText) {
-        this.setState({ shareText });
-    }
+  set shareText(shareText) {
+    this.setState({ shareText });
+  }
 
-    get shareUrl() {
-        return this.state.shareTitle;
-    }
+  get shareUrl() {
+    return this.state.shareUrl;
+  }
 
-    set shareUrl(shareUrl) {
-        this.setState({ shareUrl });
-    }
+  set shareUrl(shareUrl) {
+    this.setState({ shareUrl });
+  }
 
-    componentDidMount() {
-        super.componentDidMount();
-        this.$.inner.addEventListener('click', (evt) => {
-            if (navigator.share) {
-                navigator.share({
-                    title: this.shareTitle,
-                    text: this.shareText,
-                    url: this.shareUrl,
-                })
-                .then(() => console.log('Successful share'))
-                .catch((error) => console.log('Error sharing', error));
-            }
-            else {
-                console.log('ooops navigator.share does not exist')
-            }
-        });
-    }
+  componentDidMount() {
+    super.componentDidMount();
+    this.$.inner.addEventListener('click', (evt) => {
+      if (navigator.share) {
+        navigator.share({
+            title: this.shareTitle,
+            text: this.shareText,
+            url: this.shareUrl,
+        })
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+      }
+      else {
+        console.log('ooops navigator.share does not exist')
+      }
+    });
+  }
+
+  get [symbols.template]() {
+    return template.concat(super[symbols.template], template.html`
+      <slot name="buttonInner"></slot>
+      <slot name="fallback"></slot>
+    `);
+  }
 }
 
 
